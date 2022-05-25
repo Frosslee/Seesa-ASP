dd6fb6b5486d04b62b6f05368c79ea7b02beb78d
Frosslee <91604209+Frosslee@users.noreply.github.com>
2022-05-25 15:12:50 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,15 +2,15 @@ for (int i = 1; i <= 100; i++)
 {
     if (i % 3 == 0 && i % 5 == 0)
     {
-        Console.WriteLine("FizzBuzz " + i);
+        Console.WriteLine("FizzBuzz ");
     }
     else if (i % 3 == 0)
     {
-        Console.WriteLine( "Fizz " + i);
+        Console.WriteLine( "Fizz ");
     }
     else if (i % 5 == 0)
     {
-        Console.WriteLine("Buzz " + i);
+        Console.WriteLine("Buzz ");
     }
     else
     {
